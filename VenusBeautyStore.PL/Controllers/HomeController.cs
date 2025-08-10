73e9144f44fec135b4f81eb7eee33f57a8cd5115
Aharón Guzmán Guzmán <aharongg@hotmail.com>
2025-08-09 23:59:43 -0600

    grid productos ok

--- VenusBeautyStore.PL/Controllers/HomeController.cs
+++ VenusBeautyStore.PL/Controllers/HomeController.cs
@@ -1,27 +1,88 @@
+//using Microsoft.AspNetCore.Authorization;
+//using Microsoft.AspNetCore.Mvc;
+//using System.Diagnostics;
+//using VenusBeautyStore.PL.Models;
+
+//namespace VenusBeautyStore.PL.Controllers
+//{
+//    [AllowAnonymous]
+//    public class HomeController : Controller
+//    {
+//        private readonly ILogger<HomeController> _logger;
+
+//        public HomeController(ILogger<HomeController> logger)
+//        {
+//            _logger = logger;
+//        }
+
+//        public IActionResult Index()
+//        {
+//            return View();
+//        }
+
+//        public IActionResult Privacy()
+//        {
+//            return View();
+//        }
+
+//        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+//        public IActionResult Error()
+//        {
+//            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+//        }
+//    }
+//}
 using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using VenusBeautyStore.PL.Models;
+using VenusBeauty.BLL.Services;
+using VenusBeauty.DAL.Entities;
 
 namespace VenusBeautyStore.PL.Controllers
 {
+    [AllowAnonymous]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IProductoService _productoService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IProductoService productoService)
         {
             _logger = logger;
+            _productoService = productoService;
         }
 
-        public IActionResult Index()
+        // Home con “Productos destacados”
+        [HttpGet]
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var productos = await _productoService.ObtenerProductosAsync();
+
+            var destacados = productos
+                .Where(p => p.Activo && !string.IsNullOrWhiteSpace(p.ImagenUrl))
+                .OrderByDescending(p => p.IdProducto)
+                .Take(6)
+                .ToList();
+
+            // La vista Index recibirá IEnumerable<Producto>
+            return View(destacados);
         }
 
-        public IActionResult Privacy()
+        // (Para después) Página con todos los productos activos
+        [HttpGet]
+        public async Task<IActionResult> Productos()
         {
-            return View();
+            var productos = await _productoService.ObtenerProductosAsync();
+            var activos = productos
+                .Where(p => p.Activo)
+                .OrderByDescending(p => p.IdProducto)
+                .ToList();
+
+            return View(activos); // haremos la vista luego
         }
+
+        public IActionResult Privacy() => View();
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ VenusBeautyStore.PL/Models/HomeIndexVM.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using VenusBeauty.DAL.Entities;
+
+namespace VenusBeautyStore.PL.Models
+{
+    public class HomeIndexVM
+    {
+        public List<Producto> Productos { get; set; } = new();
+    }
+}
